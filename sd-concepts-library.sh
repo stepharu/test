615b6e2fc770d09ff1d9b38d64a6536840d5ae97
Stepharu_ <36410506+stepharu@users.noreply.github.com>
2022-11-16 04:10:44 -0800

    Add files via upload

---
+++ sd-concepts-library.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+[[ "${TRACE-0}" == "1" ]] && set -o xtrace
+
+HUGGINGFACE_TOKEN=""
+
+mkdir -p embeddings
+
+index=0
+
+curl -sS "https://huggingface.co/api/models?author=sd-concepts-library" | jq -c ".[]" | while read model; do
+  id="$(echo $model | jq -r ".id")"
+  name="${id##*/}"
+  repo="https://huggingface.co/$id"
+  repo_dir="repos/$id"
+
+  # if [[ -d "$repo_dir/.git" ]]; then
+  #   (cd "$repo_dir" && git pull --quiet)
+  # else
+  #   rm -rf "$repo_dir"
+  #   git clone --quiet $repo "$repo_dir"
+  # fi
+
+  index=$((index+1))
+
+  curl -fsSL \
+    -H "Authorization: Bearer $HUGGINGFACE_TOKEN" \
+    -o "embeddings/$name.pt" \
+    "$repo/resolve/main/learned_embeds.bin" && continue
+
+  curl -fsSL \
+    -H "Authorization: Bearer $HUGGINGFACE_TOKEN" \
+    -o "embeddings/$name.pt" \
+    "$repo/resolve/main/$name.bin" && continue
+
+  echo "FAILED!"
+done
